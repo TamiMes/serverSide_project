c57454f389d118ad807032573b3919327288f543
Tami Mesengiser <TamiM@feed-u.co>
2025-02-11 22:40:30 +0200

    Deleted functions in comments from app.js

--- app.js
+++ app.js
@@ -49,21 +49,7 @@ app.use('/', getUserdataRouter);
 app.use('/', addCostItem);
 app.use('/', reportRouter);
 
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
 
-// error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
 
 // Catch 404 and return JSON response
 app.use((req, res) => {
