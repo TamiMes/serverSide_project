37684b7fa4ad3acc1ec2e0a92fc6e1e57d618dd7
Tami Mesengiser <TamiM@feed-u.co>
2025-02-12 17:08:05 +0200

    added JSDOCS in getReport

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 /**
- * Initialize routes and middleware for the app.
+ * Set up routes
  */
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
